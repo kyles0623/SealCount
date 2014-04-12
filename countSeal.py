1f89bfe2dcefb8f4de4b3d8734b9729b07782a70
kyles0623 <kyles0623@gmail.com>
2014-04-12 03:28:19 -0400

    Create countSeal.py

---
+++ countSeal.py
@@ -0,0 +1,26 @@
+import praw
+from sys import exit
+import string
+from pprint import pprint
+
+r = praw.Reddit(user_agent='Testing some things')
+
+submission = r.get_submission(submission_id='22t2vs')
+
+
+def get_count(comment):
+	count = 0
+	if(hasattr(comment, 'body') and string.find(comment.body,'seal')):
+		count = count + 1
+	if(hasattr(comment, 'replies')):
+		for reply in comment.replies:
+			count = count + get_count(reply)
+	return count
+
+
+
+count = 0
+for comment in submission.comments:
+	count = count + get_count(comment)
+
+print count
